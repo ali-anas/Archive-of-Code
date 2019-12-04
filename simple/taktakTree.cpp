4b74b8200e616fe777e81948fe7ca10539ec01bd
ali-anas <infoalmora786@gmail.com>
2019-12-05 00:15:29 +0530

    added simple with taktakTree.cpp & totalArea.cpp

---
+++ simple/taktakTree.cpp
@@ -0,0 +1,33 @@
+// problem -> orac.amt.edu.au/cgi-bin/train/problem-pl?problemid=382&set=simple2
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool equallyDistributed(int fruits) {
+	if((fruits % 11) == 1) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+int main() {
+	// take input from input file.
+	// write output to output file.
+	// freopen("taktakin.txt","r",stdin);
+	// freopen("taktakout","w",stdout);
+
+	// take input
+	int fruits;
+	cin >> fruits;
+	int moonCounts = 0;
+	while(!equallyDistributed(fruits)) {
+		moonCounts += 1;
+		fruits = 2 * fruits;
+	}
+
+	cout << moonCounts << " " << fruits << endl;
+
+	return 0;
+}
